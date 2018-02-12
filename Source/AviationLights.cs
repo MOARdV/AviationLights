167eeb1adfb0af128d9bc05afb5dd97c08a5365d
MOARdV <cwebb@babylonbycandlelight.com>
2018-02-12 09:30:57 -0600

    Interim checkin
    Issue #4

--- Source/AviationLights.cs
+++ Source/AviationLights.cs
@@ -2,13 +2,13 @@ using System;
 using UnityEngine;
 
 //Originally made by RPGprayer, edited by BigNose, Why485, GROOV3ST3R, JDP and J.Random
-// Fixes for KSP 1.1 applied by MOARdV.
+// Fixes for KSP 1.1, additional fixes, and refactoring by MOARdV.
 //License: This file contains code from RPGprayers "Position/Navigation Lights". Used with permission.
 namespace AviationLights
 {
-    public static class navLightStates
+    public class ModuleNavLight : PartModule
     {
-        public enum navLightState
+        public enum NavLightState
         {
             Off = 0,
             Flash = 1,
@@ -16,23 +16,19 @@ namespace AviationLights
             Interval = 3,
             On = 4
         }
-    }
 
-    public class ModuleNavLight : PartModule
-    {
         protected Color _navLightColor;
 
         [KSPField(isPersistant = true)]
         public int navLightSwitch = 0;
+        private NavLightState navLightState = NavLightState.Off;
+
+        [KSPField]
+        public float intensity = 0.67f;
 
         private double _lastTimeFired;
         private GameObject LightOffsetParent;
         private Light mainLight;
-        private Light glowLight;
-
-        private const float INTENSITY_GLOW = 0.33f;
-        private const float INTENSITY_OFFSET = 0.67f;
-        bool b = false, flightStarted = false;
 
         [KSPField]
         public string Resource = "ElectricCharge";
@@ -41,8 +37,8 @@ namespace AviationLights
         [KSPField]
         public float EnergyReq = 0.0f;
 
-        [KSPField]
-        public float IntervalFlashMode = 0.0f;
+        private float nextInterval = 0.0f;
+
         [KSPField]
         public float Interval = 0.0f;
         [KSPField]
@@ -56,9 +52,23 @@ namespace AviationLights
         [KSPField]
         public Vector3 LightOffset = new Vector3(0.33f, 0.0f, 0.0f);
 
+        [KSPField(guiActive = true, guiActiveEditor = true, guiName = "#AL_LightMode")]
+        public string modeString;
+
+        private int flashCounter = 0;
+
         public override void OnStart(PartModule.StartState state)
         {
-            if (state == StartState.Editor) return;
+            if (navLightSwitch < 0 || navLightSwitch > 4)
+            {
+                navLightSwitch = 0;
+            }
+
+            if (state == StartState.Editor)
+            {
+                UpdateMode(); // Update the mode string.
+                return;
+            }
 
             try
             {
@@ -69,9 +79,8 @@ namespace AviationLights
                 resourceId = PartResourceLibrary.ElectricityHashcode;
             }
 
-            flightStarted = true;
-
             _navLightColor = new Color(Color.x, Color.y, Color.z);
+            // XXX Refactor this 
             _lastTimeFired = Planetarium.GetUniversalTime();
 
             // Parent for main illumination light, used to move it slightly above the light.
@@ -86,52 +95,52 @@ namespace AviationLights
             mainLight.color = _navLightColor;
             mainLight.intensity = 0.0f;
 
-            // Glow Illumination light
-            glowLight = base.gameObject.AddComponent<Light>();
-            glowLight.color = _navLightColor;
-            glowLight.intensity = 0.0f;
+            UpdateMode();
         }
 
         public override void OnUpdate()
         {
-            if (!flightStarted) return;
-
-            switch (navLightSwitch)
+            if (!HighLogic.LoadedSceneIsFlight)
             {
-                case (int)navLightStates.navLightState.Off:
-                    // Lights go to 'Off' mode
-                    mainLight.intensity = 0.0f;
-                    glowLight.intensity = 0.0f;
-                    break;
-                case (int)navLightStates.navLightState.Flash:
-                    // Lights go to 'Flash' mode
-                    FlashBasedSwitcher(IntervalFlashMode);
-                    break;
-                case (int)navLightStates.navLightState.DoubleFlash:
-                    // Lights go to 'Double Flash' mode
-                    DoubleFlashBasedSwitcher(IntervalFlashMode);
-                    break;
-                case (int)navLightStates.navLightState.Interval:
-                    // Lights go to 'Interval' mode
-                    IntervalBasedSwitcher(Interval);
-                    break;
-                case (int)navLightStates.navLightState.On:
-                    // Lights go to 'On' mode
-                    mainLight.intensity = INTENSITY_OFFSET;
-                    glowLight.intensity = INTENSITY_GLOW;
-                    break;
+                return;
             }
 
-            // Energy requirements check: if the light is not off and requires resources, request resource. If returned resource is less than requested; turn off
-            if (navLightSwitch > 0 && EnergyReq > 0.0f && TimeWarp.deltaTime > 0.0f)
+            if (navLightState != NavLightState.Off && EnergyReq > 0.0f && TimeWarp.deltaTime > 0.0f)
             {
                 if (vessel.RequestResource(part, resourceId, EnergyReq * TimeWarp.deltaTime, true) < EnergyReq * TimeWarp.deltaTime * 0.5f)
                 {
-                    navLightSwitch = (int)navLightStates.navLightState.Off;
+                    // XXX Refactor this - don't change mode, just slam intensity to off.
+                    // Lights out ... no power
+                    navLightSwitch = (int)NavLightState.Off;
+                    UpdateMode();
                 }
+            }
+
+            switch (navLightState)
+            {
+                case NavLightState.Off:
+                case NavLightState.On:
+                    // No-op - taken care of when mode was selected.
+                    break;
+                case NavLightState.Flash:
+                    // Lights go to 'Flash' mode
+                    FlashBasedSwitcher();
+                    break;
+                case NavLightState.DoubleFlash:
+                    // Lights go to 'Double Flash' mode
+                    DoubleFlashBasedSwitcher();
+                    break;
+                case NavLightState.Interval:
+                    // Lights go to 'Interval' mode
+                    IntervalBasedSwitcher();
+                    break;
             }
         }
 
+        /// <summary>
+        /// Provide VAB info.
+        /// </summary>
+        /// <returns></returns>
         public override string GetInfo()
         {
             if (EnergyReq > 0.0f)
@@ -166,44 +175,62 @@ namespace AviationLights
             }
         }
 
-        private void FlashBasedSwitcher(float FlashOn)
+        private void UpdateLights(bool lightsOn)
         {
-            if (_lastTimeFired < Planetarium.GetUniversalTime() - FlashOn)
+            if (lightsOn)
             {
-                b = !b;
-                IntervalFlashMode = b ? this.FlashOn : FlashOff;
-                _lastTimeFired = Planetarium.GetUniversalTime();
-                mainLight.intensity = (mainLight.intensity == INTENSITY_OFFSET) ? 0.0f : INTENSITY_OFFSET;
-                glowLight.intensity = (glowLight.intensity == INTENSITY_GLOW) ? 0.0f : INTENSITY_GLOW;
+                mainLight.intensity = intensity;
+            }
+            else
+            {
+                mainLight.intensity = 0.0f;
             }
         }
 
-        private void DoubleFlashBasedSwitcher(float FlashOn)
+        // Periodic strobe with differing on and off times.
+        private void FlashBasedSwitcher()
         {
-            mainLight.intensity = 0.0f;
-            glowLight.intensity = 0.0f;
-
-            if (_lastTimeFired < Planetarium.GetUniversalTime() - FlashOn)
+            float interval = nextInterval;
+            if (_lastTimeFired < Planetarium.GetUniversalTime() - interval)
             {
-                b = !b;
-                IntervalFlashMode = b ? this.FlashOn : FlashOff;
+                flashCounter = (flashCounter + 1) & 1;
+
+                nextInterval = ((flashCounter & 1) == 1) ? FlashOn : FlashOff;
                 _lastTimeFired = Planetarium.GetUniversalTime();
-                mainLight.intensity = (mainLight.intensity == INTENSITY_OFFSET) ? 0.0f : INTENSITY_OFFSET;
-                glowLight.intensity = (glowLight.intensity == INTENSITY_GLOW) ? 0.0f : INTENSITY_GLOW;
+                UpdateLights((flashCounter & 1) == 1);
             }
         }
 
-        private void IntervalBasedSwitcher(float Interval)
+        // Periodic strobe with two strobes of equal length close together.
+        private void DoubleFlashBasedSwitcher()
         {
-            if (_lastTimeFired < Planetarium.GetUniversalTime() - Interval)
+            float interval = nextInterval;
+
+            if (_lastTimeFired < Planetarium.GetUniversalTime() - nextInterval)
             {
+                flashCounter = (flashCounter + 1) & 3;
+                nextInterval = (flashCounter > 0) ? FlashOn : FlashOff;
+
                 _lastTimeFired = Planetarium.GetUniversalTime();
-                mainLight.intensity = (mainLight.intensity == INTENSITY_OFFSET) ? 0.0f : INTENSITY_OFFSET;
-                glowLight.intensity = (glowLight.intensity == INTENSITY_GLOW) ? 0.0f : INTENSITY_GLOW;
+                UpdateLights((flashCounter & 1) == 1);
             }
         }
 
-        [KSPAction("#AL_LightToggle", KSPActionGroup.None)]
+        // Periodic strobe with equal on and off times.
+        private void IntervalBasedSwitcher()
+        {
+            float interval = Interval;
+            if (_lastTimeFired < Planetarium.GetUniversalTime() - interval)
+            {
+                flashCounter = (flashCounter + 1) & 1;
+
+                _lastTimeFired = Planetarium.GetUniversalTime();
+                UpdateLights((flashCounter & 1) == 1);
+            }
+        }
+
+        //--- "Toggle" action group events
+        [KSPAction("#autoLOC_6001405", KSPActionGroup.None)]
         public void LightToggle(KSPActionParam param)
         {
             LightOnEvent();
@@ -230,97 +257,120 @@ namespace AviationLights
         [KSPAction("#AL_CycleModes", KSPActionGroup.None)]
         public void Cycle(KSPActionParam param)
         {
-            _lastTimeFired = 0.0;
-            b = false;
-
             navLightSwitch = (navLightSwitch + 1) % 5;
-            //if (navLightSwitch == 4)
-            //{
-            //    navLightSwitch = 0;
-            //}
-            //else
-            //{
-            //    navLightSwitch++;
-            //}
+
+            UpdateMode();
         }
 
-        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_SetFlash")]
-        public void LightFlashEvent()
+        //--- "Set" action group events
+        [KSPAction("#autoLOC_6001406", KSPActionGroup.None)]
+        public void LightOnAction(KSPActionParam param)
+        {
+            navLightSwitch = (int)NavLightState.On;
+
+            UpdateMode();
+        }
+
+        [KSPAction("#AL_SetFlash", KSPActionGroup.None)]
+        public void LightFlashAction(KSPActionParam param)
+        {
+            navLightSwitch = (int)NavLightState.Flash;
+
+            UpdateMode();
+        }
+
+        [KSPAction("#AL_SetDoubleFlash", KSPActionGroup.None)]
+        public void LightDoubleFlashAction(KSPActionParam param)
+        {
+            navLightSwitch = (int)NavLightState.DoubleFlash;
+
+            UpdateMode();
+        }
+
+        [KSPAction("#AL_SetInterval", KSPActionGroup.None)]
+        public void LightIntervalAction(KSPActionParam param)
+        {
+            navLightSwitch = (int)NavLightState.Interval;
+
+            UpdateMode();
+        }
+
+        [KSPAction("#autoLOC_6001407", KSPActionGroup.None)]
+        public void LightOffAction(KSPActionParam param)
+        {
+            navLightSwitch = (int)NavLightState.Off;
+
+            UpdateMode();
+        }
+
+        private void UpdateMode()
         {
             _lastTimeFired = 0.0;
-            b = false;
-            if (mainLight != null)
+            flashCounter = 0;
+
+            switch (navLightSwitch)
             {
-                mainLight.intensity = 0.0f;
-                glowLight.intensity = 0.0f;
+                case (int)NavLightState.Off:
+                default:
+                    navLightSwitch = (int)NavLightState.Off; // Trap invalid values from the config file
+                    navLightState = NavLightState.Off;
+                    modeString = KSP.Localization.Localizer.GetStringByTag("#autoLOC_6001073");
+                    break;
+                case (int)NavLightState.Flash:
+                    navLightState = NavLightState.Flash;
+                    modeString = KSP.Localization.Localizer.GetStringByTag("#AL_ModeFlash");
+                    break;
+                case (int)NavLightState.DoubleFlash:
+                    navLightState = NavLightState.DoubleFlash;
+                    modeString = KSP.Localization.Localizer.GetStringByTag("#AL_ModeDoubleFlash");
+                    break;
+                case (int)NavLightState.Interval:
+                    navLightState = NavLightState.Interval;
+                    modeString = KSP.Localization.Localizer.GetStringByTag("#AL_ModeInterval");
+                    break;
+                case (int)NavLightState.On:
+                    navLightState = NavLightState.On;
+                    modeString = KSP.Localization.Localizer.GetStringByTag("#autoLOC_6001074");
+                    break;
             }
 
-            if (navLightSwitch == 1)
+            if (mainLight != null)
             {
-                navLightSwitch = 0;
-            }
-            else
-            {
-                navLightSwitch = 1;
+                mainLight.intensity = (navLightState == NavLightState.On) ? intensity : 0.0f;
             }
         }
 
-        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_SetDoubleFlash")]
-        public void LightDoubleFlashEvent()
-        {
-            _lastTimeFired = 0.0;
-            b = false;
-            if (mainLight != null)
-            {
-                mainLight.intensity = 0.0f;
-                glowLight.intensity = 0.0f;
-            }
-
-            if (navLightSwitch == 2)
-            {
-                navLightSwitch = 0;
-            }
-            else
-            {
-                navLightSwitch = 2;
-            }
-        }
-
-        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_SetInterval")]
-        public void LightIntervalEvent()
-        {
-            _lastTimeFired = 0.0;
-            b = false;
-            if (mainLight != null)
-            {
-                mainLight.intensity = 0.0f;
-                glowLight.intensity = 0.0f;
-            }
-
-            if (navLightSwitch == 3)
-            {
-                navLightSwitch = 0;
-            }
-            else
-            {
-                navLightSwitch = 3;
-            }
-        }
-
-        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_SetLight")]
+        //--- Part context menu events
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#autoLOC_6001405")]
         public void LightOnEvent()
         {
-            _lastTimeFired = 0.0;
-            b = false;
+            navLightSwitch = (navLightSwitch == (int)NavLightState.On) ? (int)NavLightState.Off : (int)NavLightState.On;
 
-            if (navLightSwitch == 4)
-            {
-                navLightSwitch = 0;
-            }
-            else
-            {
-                navLightSwitch = 4;
-            }
+            UpdateMode();
+        }
+
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_ToggleFlash")]
+        public void LightFlashEvent()
+        {
+            navLightSwitch = (navLightSwitch == (int)NavLightState.Flash) ? (int)NavLightState.Off : (int)NavLightState.Flash;
+
+            UpdateMode();
+        }
+
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_ToggleDoubleFlash")]
+        public void LightDoubleFlashEvent()
+        {
+            navLightSwitch = (navLightSwitch == (int)NavLightState.DoubleFlash) ? (int)NavLightState.Off : (int)NavLightState.DoubleFlash;
+
+            UpdateMode();
+        }
+
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "#AL_ToggleInterval")]
+        public void LightIntervalEvent()
+        {
+            navLightSwitch = (navLightSwitch == (int)NavLightState.Interval) ? (int)NavLightState.Off : (int)NavLightState.Interval;
+
+            UpdateMode();
         }
     }
 }
