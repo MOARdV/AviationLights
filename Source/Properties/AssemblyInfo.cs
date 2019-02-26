9f4ab9d2f52fe4b0d9cbe8c596b09d00da89cd4c
MOARdV <cwebb@babylonbycandlelight.com>
2019-02-25 18:15:20 -0600

    Bump to v4.0.7
    Add the French localization, and make sure all of the version data is
    actually synchronized.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.6")]
+[assembly: AssemblyVersion("4.0.7")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
